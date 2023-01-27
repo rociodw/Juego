cf09cb70d8c2dcdf61051c307d3ef4fb8e2e1f6c
rociodw <rocio.dw@outlook.es>
2023-01-27 21:17:27 +0100

    overflow

--- script.js
+++ script.js
@@ -83,4 +83,9 @@ jugarAgain.addEventListener('click', function(){
     resultadoComputadora.innerHTML=0
     resultadoUsuario.innerHTML= 0
 })
-
+const appHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  window.addEventListener('resize', appHeight)
+  appHeight()
