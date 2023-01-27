05c43a3fd0a16c5a07da02faf16aed728f7cd5bc
rociodw <rocio.dw@outlook.es>
2023-01-27 19:27:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+var computadora=document.getElementById('computadora')
+var usuario =document.querySelectorAll('.btn-usuario')
+var opciones=['piedra', 'papel', 'tijera']
+var ganador=document.getElementById('ganador')
+var resultadoUsuario=document.getElementById('puntos-usuario')
+var resultadoComputadora=document.getElementById('puntos-computadora')
+var seleccionUsuario=document.getElementById('seleccion-usuario')
+var puntosComputadora=1
+var puntosUsuario=1
+var contenedorGanador=document.getElementById('contenedor-ganador')
+var closeGanador=document.getElementById('close-ganador')
+var ganador=document.getElementById('ganador')
+var jugarAgain=document.getElementById('jugar-again')
+var lottie=document.getElementById('contenedor-lottie')
+
+// document.oncontextmenu = function(){return false}
+
+usuario.forEach(boton => {
+    boton.addEventListener('click',function(){
+    var computadoraResultado=opciones[Math.floor(Math.random()*3)]
+    computadora.innerHTML= '<img src="/img/red-'+computadoraResultado+'.png" alt="">'
+    var usuario=boton.value
+    console.log('u'+usuario);
+    seleccionUsuario.innerHTML ='<img src="/img/green-'+usuario+'.png" alt="">'
+    seleccionUsuario.classList.remove('igual')
+    computadora.classList.remove('igual')
+    switch (true) {
+        case (usuario===computadoraResultado):
+            console.log('igual');
+            seleccionUsuario.classList.add('igual')
+            computadora.classList.add('igual')
+            break;
+        case (usuario==='piedra' && computadoraResultado==='tijera'):
+            console.log('Has ganado!');
+            resultadoUsuario.innerHTML= puntosUsuario++
+            break;
+        case (usuario==='papel' && computadoraResultado==='piedra'):
+            console.log('Has ganado!');
+            resultadoUsuario.innerHTML= puntosUsuario++
+            break;
+        case (usuario==='tijera' && computadoraResultado==='papel'):
+            console.log('Has ganado!');
+            resultadoUsuario.innerHTML= puntosUsuario++
+            break;
+        default:
+            resultadoComputadora.innerHTML=puntosComputadora++
+            break;
+    }
+
+
+    if(puntosComputadora===6){
+        ganador.innerHTML='Perdiste esta ronda, ¿quieres jugar otra vez?'
+        ganador.style.border='4px solid rgb(152, 64, 80)'
+        contenedorGanador.style.display='block'
+        puntosComputadora=0
+        lottie.innerHTML='<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script><lottie-player src="https://lottie.host/9ac95d94-b484-4a89-a34d-22ebebfd3b63/RcGuqnJXFB.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>'
+        contenedorGanador.style.background='linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(222, 123, 141) 71%)';
+        jugarAgain.style.boxShadow='1px 6px 6px rgb(152, 64, 80)'
+        
+    }
+    
+    if(puntosUsuario===6){
+        ganador.innerHTML='¡Ganaste! ¿quieres jugar otra vez?'
+        contenedorGanador.style.display='block'
+        puntosUsuario=0
+        lottie.innerHTML='<lottie-player src="https://lottie.host/1e47e80b-1eb5-4e24-a574-f6a1dce34a9c/MjtKzfHdNJ.json" background="transparent" speed="1" style="width: 70vw; height: 300px;" loop autoplay></lottie-player>'
+        
+    }
+    
+
+})
+
+})
+
+// closeGanador.addEventListener('click', function(){
+//     contenedorGanador.style.display='none'
+//     resultadoComputadora.innerHTML=0
+//     resultadoUsuario.innerHTML= 0
+// })
+
+jugarAgain.addEventListener('click', function(){
+    contenedorGanador.style.display='none'
+    resultadoComputadora.innerHTML=0
+    resultadoUsuario.innerHTML= 0
+})
